a086c1e002247ed0e1af0f53b3b53e1bcfe06bfb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use dashboard parameters with the Expression format condition
+
+
+<p>The Expression format condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionExpressiontopic">FormatConditionExpression</a>) allows you to use complex conditions to apply formatting.<br />This example demonstrates how to pass a dashboard parameter to an expression used to apply conditional formatting. You can specify the required parameter value to apply formatting dynamically.</p>
+
+<br/>
+
+
---
+++ VB/Dashboard_ConditionalFormatting_Grid/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports DevExpress.DashboardCommon
+
+Namespace Grid_ExpressionCondition
+    Partial Public Class Form1
+        Inherits DevExpress.XtraEditors.XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            Dim dashboard As New Dashboard()
+            dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+            dashboardViewer1.Dashboard = dashboard
+            Dim grid As GridDashboardItem =
+                CType(dashboard.Items("gridDashboardItem1"), GridDashboardItem)
+            Dim extendedPrice As GridMeasureColumn = CType(grid.Columns(1), GridMeasureColumn)
+            extendedPrice.Measure.UniqueId = "extendedPrice"
+
+            Dim priceParameter As New DashboardParameter()
+            priceParameter.LookUpSettings = Nothing
+            priceParameter.Name = "priceParameter"
+            priceParameter.Type = GetType(Decimal)
+            priceParameter.Value = 150000
+            priceParameter.Description = "Format values that are greater than"
+            dashboard.Parameters.Add(priceParameter)
+
+            Dim greaterThanRule As New GridItemFormatRule(extendedPrice)
+            Dim greaterThanCondition As New FormatConditionExpression()
+            greaterThanCondition.Expression = "extendedPrice > [Parameters.priceParameter]"
+            greaterThanCondition.StyleSettings =
+                New AppearanceSettings(FormatConditionAppearanceType.PaleGreen)
+            greaterThanRule.ApplyToRow = True
+            greaterThanRule.Condition = greaterThanCondition
+
+            grid.FormatRules.AddRange(greaterThanRule)
+        End Sub
+    End Class
+End Namespace
