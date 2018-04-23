b20e4b437c9ae7fb4de3bd03a176dd0cff79698f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_ConditionalFormatting_Grid/Form1.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.DashboardCommon;
+
+namespace Grid_ExpressionCondition {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        public Form1() {
+            InitializeComponent();
+            Dashboard dashboard = new Dashboard(); dashboard.LoadFromXml(@"..\..\Data\Dashboard.xml");
+            dashboardViewer1.Dashboard = dashboard;
+            GridDashboardItem grid = (GridDashboardItem)dashboard.Items["gridDashboardItem1"];
+            GridMeasureColumn extendedPrice = (GridMeasureColumn)grid.Columns[1];
+            extendedPrice.Measure.ID = "extendedPrice";
+
+            DashboardParameter priceParameter = new DashboardParameter();
+            priceParameter.LookUpSettings = null;
+            priceParameter.Name = "priceParameter";
+            priceParameter.Type = typeof(decimal);
+            priceParameter.Value = 150000;
+            priceParameter.Description = "Format values that are greater than";
+            dashboard.Parameters.Add(priceParameter);
+
+            GridItemFormatRule greaterThanRule = new GridItemFormatRule(extendedPrice);
+            FormatConditionExpression greaterThanCondition = new FormatConditionExpression();
+            greaterThanCondition.Expression = "extendedPrice > [Parameters.priceParameter]";
+            greaterThanCondition.StyleSettings = 
+                new AppearanceSettings(FormatConditionAppearanceType.PaleGreen);
+            greaterThanRule.ApplyToRow = true;
+            greaterThanRule.Condition = greaterThanCondition;
+
+            grid.FormatRules.AddRange(greaterThanRule);
+        }
+    }
+}
